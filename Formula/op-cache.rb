423bbe5f3501d6a7749ecd9d21c5d6453ed6f0dd
Simon Lansing <tech+github@lansing.nrw>
2026-03-06 00:37:21 +0100

    chore: update username to simlans

--- Formula/op-cache.rb
+++ Formula/op-cache.rb
@@ -1,26 +1,26 @@
 class OpCache < Formula
   desc "Fast cache for 1Password CLI reads"
   homepage "https://github.com/simlans/direnv-libs"
-  version "1.1.0"
+  version "2.0.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/simlans/direnv-libs/releases/download/v1.1.0/op-cache-aarch64-apple-darwin.tar.gz"
-      sha256 "8e33dda54a186e8b3713d3b5f6de60d594a0a60e2b912d051da534f785eb39a1"
+      url "https://github.com/simlans/direnv-libs/releases/download/v2.0.0/op-cache-aarch64-apple-darwin.tar.gz"
+      sha256 "347def849bb03a24b3b2f2dc082aa03bcfdb7cce4ce88206a90640e267407ee0"
     else
-      url "https://github.com/simlans/direnv-libs/releases/download/v1.1.0/op-cache-x86_64-apple-darwin.tar.gz"
-      sha256 "4b7c64d4c186861f0ae45eebed87542e0d5eadc8fca5499f4f82f49e0bed6fb7"
+      url "https://github.com/simlans/direnv-libs/releases/download/v2.0.0/op-cache-x86_64-apple-darwin.tar.gz"
+      sha256 "9d906f8dfe2eec700feae8e8c7cc695f306dc10a3aa94e1b0d482697d8ca2c5e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/simlans/direnv-libs/releases/download/v1.1.0/op-cache-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "2dbbc1fd11d54194d6fe2505515342a0943ad60b490ab0a3cb09c68562a14af6"
+      url "https://github.com/simlans/direnv-libs/releases/download/v2.0.0/op-cache-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "9c9d8d7d395c1be5b7b6e1be9602f3076cdbe1bfd3b8c5e4698c5477be5af378"
     else
-      url "https://github.com/simlans/direnv-libs/releases/download/v1.1.0/op-cache-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "3a320c5d9e2492c40734975cf23922d28d7ac86810a354093667eb9174c11b76"
+      url "https://github.com/simlans/direnv-libs/releases/download/v2.0.0/op-cache-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "4feb03a4dd9e43ca6e2581cdc5b32598948a7b12373b27f0c342106ad2417bf4"
     end
   end
 
